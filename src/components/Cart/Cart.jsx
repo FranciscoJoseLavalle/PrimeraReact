c8b0ef80d1595cdc0d8aeac5262c2f75e8f0aeee
Francisco Jose Lavalle <franlavalle@hotmail.com>
2022-06-04 18:47:46 -0300

    Agrego compra

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,17 +1,54 @@
 import './Cart.css';
 import { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { getFirestore, collection, getDocs, query, where, addDoc } from 'firebase/firestore';
 import { CartContext } from '../../context/CartContext';
 import CartItem from '../CartItem/CartItem';
 
 function Cart() {
     const { cartList, vaciarCarrito, disminuirCantidad, aumentarCantidad, precioTotal, setPrecioTotal, setProductosTotales, productosTotales, borrar } = useContext(CartContext);
 
-
-
     setPrecioTotal(cartList.map(element => element.cantidad * element.precio).reduce((anterior, siguiente) => anterior + siguiente, 0))
 
     setProductosTotales(cartList.map(element => element.cantidad).reduce((anterior, siguiente) => anterior + siguiente, 0))
+
+    const [data, setData] = useState({
+        name: '',
+        email: '',
+        phone: ''
+    })
+
+    function handleInputChange(event) {
+        console.log(data)
+        setData({
+            ...data,
+            [event.target.name] : event.target.value
+        })
+    }
+
+    function sendOrder() {
+        let order = {};
+
+        order.buyer = data;
+        order.items = cartList.map(cartItem => {
+            const id = cartItem.id;
+            const name = cartItem.nombre;
+            const price =  cartItem.precio;
+            const quantity = cartItem.cantidad;
+
+            return {id, name, price, quantity}
+        })
+        order.total = precioTotal;
+
+        console.log(order)
+
+        const db = getFirestore();
+        const queryCollectionOrders = collection(db, 'orders')
+        addDoc(queryCollectionOrders, order)
+        .then(resp => console.log(resp))
+        .catch(err => console.log(err))
+        .finally(vaciarCarrito())
+    }
 
     if (!productosTotales) {
         return (
@@ -27,16 +64,32 @@ function Cart() {
         return (
             <div className='carrito'>
                 <h2>Carrito</h2>
+                <form onSubmit={sendOrder}>
+                    <div className='inputDiv'>
+                        <label htmlFor="name">Nombre</label>
+                        <input type="text" htmlFor="name" id='name' placeholder='Nombre*' name='name' onChange={handleInputChange} required/>
+                    </div>
+                    <div className='inputDiv'>
+                        <label htmlFor="email">Email</label>
+                        <input type="email" htmlFor="email" id='email' placeholder='Email*' name='email' onChange={handleInputChange} required/>
+                    </div>
+                    <div className='inputDiv'>
+                        <label htmlFor="phone">Teléfono</label>
+                        <input type="number" htmlFor="phone" id='phone' placeholder='Teléfono*' name='phone' onChange={handleInputChange} required/>
+                    </div>
+                    <button type='submit' className='comprar'>Comprar</button>
+                </form>
                 <div className='productosCont'>
                     {cartList.map(producto => <CartItem 
                     key={producto.id} 
                     producto={producto}
                     aumentarCantidad={aumentarCantidad}
                     disminuirCantidad={disminuirCantidad}
-                    borrar={borrar}/>)}
+                    borrar={borrar}
+                    sendOrder={sendOrder}/>)}
                 </div>
                 <p>Precio final: ${precioTotal}</p>
-
+                
                 <button className='vaciar' onClick={vaciarCarrito}>Vaciar Carrito</button>
             </div>
         )
